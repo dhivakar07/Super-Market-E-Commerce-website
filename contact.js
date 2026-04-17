ec98bb5820d2eebc7e48ed14d03f56a46ea65b2f
venka91 <venka9124@gmail.com>
2026-04-17 23:32:34 +0530

    updated products

--- contact.js
+++ contact.js
@@ -1,12 +1,45 @@
-//Side Navbar
-const sidenavbar = document.getElementById("sidenavbar");
-const menuicon = document.getElementById("menuicon");
-const closenavbar = document.getElementById("closenavbar");
+// FORM VALIDATION
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
 
-menuicon.addEventListener("click", () => {
-  sidenavbar.style.right = "0";
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let phone = document.getElementById("phone").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  let successMsg = document.getElementById("successMsg");
+
+  successMsg.innerText = "";
+
+  if (name === "" || email === "" || phone === "" || message === "") {
+    alert("Please fill all fields!");
+    return;
+  }
+
+  if (!email.includes("@")) {
+    alert("Enter valid email!");
+    return;
+  }
+
+  if (phone.length < 10) {
+    alert("Enter valid phone number!");
+    return;
+  }
+
+  successMsg.innerText = "Message sent successfully!";
+  document.getElementById("contactForm").reset();
 });
 
-closenavbar.addEventListener("click", () => {
-  sidenavbar.style.right = "-40%";
+
+// SIDE NAVBAR FUNCTION
+let menu = document.getElementById("menuicon");
+let sidenav = document.getElementById("sidenavbar");
+let close = document.getElementById("closenavbar");
+
+menu.addEventListener("click", function () {
+  sidenav.style.right = "0";
+});
+
+close.addEventListener("click", function () {
+  sidenav.style.right = "-40%";
 });
