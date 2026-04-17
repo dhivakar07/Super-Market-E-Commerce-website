035d1af6bf1b53800dba37768fc1a34f6f96e8b0
dhivakar07 <dhivakar782001@gmail.com>
2026-04-17 17:45:59 +0530

    update media for product page

--- index.js
+++ index.js
@@ -82,3 +82,16 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+//Side Navbar
+const sidenavbar = document.getElementById("sidenavbar");
+const menuicon = document.getElementById("menuicon");
+const closenavbar = document.getElementById("closenavbar");
+
+menuicon.addEventListener("click", () => {
+  sidenavbar.style.right = "0";
+});
+
+closenavbar.addEventListener("click", () => {
+  sidenavbar.style.right = "-40%";
+});
--- products.js
+++ products.js
@@ -79,3 +79,16 @@ addbtn.forEach((btn) => {
     }
   });
 });
+
+//Side Navbar
+const sidenavbar = document.getElementById("sidenavbar");
+const menuicon = document.getElementById("menuicon");
+const closenavbar = document.getElementById("closenavbar");
+
+menuicon.addEventListener("click", () => {
+  sidenavbar.style.right = "0";
+});
+
+closenavbar.addEventListener("click", () => {
+  sidenavbar.style.right = "-40%";
+});
